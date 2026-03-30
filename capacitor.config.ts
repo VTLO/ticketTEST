a74ecb92097b44358ab5cf0b48f6c5576f67d281
VTLO <guillaume.rochas@gmail.com>
2026-03-30 21:46:22 +0200

    feat: Integrate Capacitor for mobile app deployment
    Adds Capacitor configuration and dependencies to enable building the M-Réso Ticket Generator application for mobile platforms (Android and iOS). This includes setting up the Capacitor configuration file, updating Android-specific build configurations, and integrating necessary Capacitor packages.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mreso.app',
+  appName: 'Mreso',
+  webDir: 'dist'
+};
+
+export default config;
